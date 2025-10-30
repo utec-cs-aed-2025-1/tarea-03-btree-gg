ac17700b32485f2619bcf0a2c12cc22a238fb32a
gianw-k <gianaedo8@gmail.com>
2025-10-30 07:46:18 -0500

    node fix

--- node.h
+++ node.h
@@ -8,13 +8,13 @@ struct Node {
   // array de keys
   TK* keys;
   // array de punteros a hijos
-  Node** children;
+  Node<TK>** children;
   // cantidad de keys
   int count;
   // indicador de nodo hoja
   bool leaf;
 
-  Node() : keys(nullptr), children(nullptr), count(0) {}
+  Node() : keys(nullptr), children(nullptr), count(0), leaf(true){}
   Node(int M) {
     keys = new TK[M - 1];
     children = new Node<TK>*[M];
