523be35f5169697e4edd72205828d8a4950213e8
gianw-k <gianaedo8@gmail.com>
2025-11-01 21:29:56 -0500

    fix killself

--- node.h
+++ node.h
@@ -23,32 +23,19 @@ struct Node {
   }
 
   void killSelf() {
-    // delete del array de keys
-    if (keys) {
-      delete[] keys;
-      keys = nullptr;
-    }
-    
-    // para el ptr array
-    if (children != nullptr){
-      // si no es hoja se borran los children 
-      if (!leaf){
-        for (int i=0; i<=count; i++){
-          if(children[i] != nullptr){
-            children[i]->killSelf();
-            delete children[i];
-            children[i]=nullptr;
-          }
+    // si no es hoja se borran los children 
+    if (!leaf) {
+      for (int i = 0; i <= count; i++) {
+        if (children[i] != nullptr) {
+          children[i]->killSelf();
         }
       }
-      
       delete[] children;
-      children=nullptr;
     }
-
-    // reset
-    count = 0;
-    leaf = true;
+    // delete del array de keys
+    if (keys != nullptr) {
+      delete[] keys;
+    }
   }
 };
 
